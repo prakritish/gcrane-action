36bea4d12e1f68315ac4b9f5ef09086c13d1bf26
Prakritish Sen Eshore <prakritish@endor.ai>
2023-01-23 22:55:52 +0530

    Multiple Repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-PROJECT=$1
-REPOSITORY=$2
-TAG=$3
-JSON_KEY=$4
+LOCATION=$1
+PROJECT=$2
+REPOSITORY_LIST=$3
+TAG=$4
+JSON_KEY=$5
 echo "$JSON_KEY" > /tmp/key.json
 SERVICE_ACCOUNT=$(jq .client_email /tmp/key.json | tr -d '"')
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=/tmp/key.json --project=$PROJECT
-IMAGES=$(gcrane ls ${REPOSITORY})
 IMAGE_LIST=()
-for image in $IMAGES
+for repository in ${REPOSITORY_LIST}
 do
-    gcrane manifest ${image}:${TAG} >/dev/null 2>&1 && IMAGE_LIST+=(${image})
+    REPOSITORY=${location}-docker.pkg.dev/${PROJECT}/${repository}
+    IMAGES=$(gcrane ls ${REPOSITORY})
+    for image in $IMAGES
+    do
+        gcrane manifest ${image}:${TAG} >/dev/null 2>&1 && IMAGE_LIST+=(${image})
+    done
 done
 echo "matrix=$(jq -Rsc 'split(" ")' <<< ${IMAGE_LIST[@]} | sed 's/\\n//g')"
 echo "matrix=$(jq -Rsc 'split(" ")' <<< ${IMAGE_LIST[@]} | sed 's/\\n//g')" >> $GITHUB_OUTPUT
