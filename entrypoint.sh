fc125dc180282d2f5462689ccccd8078a87359b0
Prakritish Sen Eshore <prakritish@endor.ai>
2023-01-23 17:35:24 +0530

    Executable!

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ JSON_KEY=$3
 echo "$JSON_KEY" > /tmp/key.json
 SERVICE_ACCOUNT=$(jq .client_email /tmp/key.json | tr -d '"')
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=/tmp/key.json --project=$PROJECT
-# IMAGES=$(gcrane ls ${REPOSITORY} | tr "\n" " ")
-# echo "$IMAGES"
-# my_images=$(jq -n --arg images "$IMAGES" '{"images": $images | split("\n")}')
-IMAGES=$(gcrane ls us-central1-docker.pkg.dev/endor-ci/private --json | jq .child[] | tr "\n" ",")
-echo "images={\"include\":[${IMAGES%,}]}"
-echo "images={\"include\":[${IMAGES%,}]}" >> $GITHUB_OUTPUT
+IMAGES=$(gcrane ls ${REPOSITORY} | tr "\n" " ")
+echo "$IMAGES"
+my_images=$(jq -n --arg images "$IMAGES" '{"images": $images | split("\n")}')
+# IMAGES=$(gcrane ls us-central1-docker.pkg.dev/endor-ci/private --json | jq .child[] | tr "\n" ",")
+echo "images=$my_images"
+echo "images=$my_images" >> $GITHUB_OUTPUT
