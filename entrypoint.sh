17fc8ee834ec128b7c9f3a366660213a64a3ee57
Prakritish Sen Eshore <prakritish@endor.ai>
2023-01-23 16:07:42 +0530

    Executable!

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ JSON_KEY=$3
 echo "$JSON_KEY" > /tmp/key.json
 SERVICE_ACCOUNT=$(jq .client_email /tmp/key.json | tr -d '"')
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=/tmp/key.json --project=$PROJECT
-IMAGES=$(gcrane ls ${REPOSITORY} | tr "\n" " ")
+# IMAGES=$(gcrane ls ${REPOSITORY} | tr "\n" " ")
 # echo "$IMAGES"
 # my_images=$(jq -n --arg images "$IMAGES" '{"images": $images | split("\n")}')
-echo "images=${IMAGES}" >> $GITHUB_OUTPUT
+IMAGES=$(gcrane ls us-central1-docker.pkg.dev/endor-ci/private --json | jq .child[] | tr "\n" ", ")
+echo "images=[${IMAGES%,}]" >> $GITHUB_OUTPUT
