bad32b00ec3cea13d0a5a290562c6fb750c5125e
Prakritish Sen Eshore <prakritish@endor.ai>
2023-01-23 16:18:02 +0530

    Executable!

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=/tmp/key.json -
 # IMAGES=$(gcrane ls ${REPOSITORY} | tr "\n" " ")
 # echo "$IMAGES"
 # my_images=$(jq -n --arg images "$IMAGES" '{"images": $images | split("\n")}')
-IMAGES=$(gcrane ls us-central1-docker.pkg.dev/endor-ci/private --json | jq .child[] | tr "\n" " ")
-echo "images=${IMAGES%,}" >> $GITHUB_OUTPUT
+IMAGES=$(gcrane ls us-central1-docker.pkg.dev/endor-ci/private --json | jq .child[] | tr "\n" ",")
+echo "images=[${IMAGES%,}]" >> $GITHUB_OUTPUT
